36b4103c36154e7f1f0223262c1892bfc80a1432
juanita <juanitaag1@gmail.com>
2019-04-26 15:23:02 -0700

    Added code to Video struct and Added Unit-testing for Video

--- GYBITG/Models/Video.swift
+++ GYBITG/Models/Video.swift
@@ -1,4 +1,4 @@
-//
+//This Video struct will represent the Video model in the application. It will have 6 properties that will make up this Video. The videoID is the String id, The description will be an optional to allow the user to take a video without writing a description. The dateTaken is the date the video was taken. The fileName is the String file, the videoDuration is the length of the video represented as a CMTime object. The videoURL is the URL of the video.
 //  Video.swift
 //  GYBITG
 //
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreVideo
+import AVFoundation
+import CoreMedia
+
+struct Video {
+    let videoID: String
+    let description: String?
+    let dateTaken: Date
+    let fileName: String
+    let videoDuration: CMTime
+    let videoURL: URL
+    
+    init(videoID: String, description: String? = nil, dateTaken: Date, fileName: String, videoDuration: CMTime, videoURL: URL){
+        self.videoID = videoID
+        self.description = description
+        self.dateTaken = dateTaken
+        self.fileName = fileName
+        self.videoDuration = videoDuration
+        self.videoURL = videoURL
+    }
+    
+}
