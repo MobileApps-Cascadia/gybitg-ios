4238c9d2ad5177801e3f52c4066798c7363e2458
juanita <juanitaag1@gmail.com>
2019-04-28 13:31:41 -0700

    comment cleanup

--- GYBITG/Models/Video.swift
+++ GYBITG/Models/Video.swift
@@ -1,4 +1,4 @@
-//This Video struct will represent the Video model in the application. It will have 6 properties that will make up this Video. The videoID is the String id, The description will be an optional to allow the user to take a video without writing a description. The dateTaken is the date the video was taken. The fileName is the String file, the videoDuration is the length of the video represented as a CMTime object. The videoURL is the URL of the video.
+//This Video struct will represent the Video model in the application. It will have 7 properties that will make up this Video. The videoID is the String id, The description will be an optional to allow the user to take a video without writing a description. The dateTaken is the date the video was taken. The fileName is the String file, the videoDuration is the length of the video represented as a CMTime object. The videoURL is the URL of the video. The userID is the id of the user associated with the Video.
 //  Video.swift
 //  GYBITG
 //
